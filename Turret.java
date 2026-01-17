5aa768c62908439cb902570905656d63a1ac306d
Anya <bearduckparty@gmail.com>
2026-01-17 09:45:07 -0500

    the actual thing

--- Turret.java
+++ Turret.java
@@ -1,5 +1,8 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
 import frc.robot.LimelightHelpers;
 import frc.robot.LimelightHelpers.LimelightResults;
 import frc.robot.LimelightHelpers.PoseEstimate;
@@ -8,6 +11,19 @@ import frc.robot.LimelightHelpers.PoseEstimate;
 
 
 public class Turret {
+
+    private final SparkMax xTurretMotion;
+    private final SparkMax yTurretMotion;
+    private final SparkMax flyWheel;
+
+    public Turret() {
+
+        xTurretMotion = new SparkMax(0, MotorType.kBrushless);
+        yTurretMotion = new SparkMax(0, MotorType.kBrushless);
+        flyWheel = new SparkMax(0, MotorType.kBrushless);
+
+    }
+
 
     
    
