c9aadffbc4013da734c45dd904d9253f45b253e4
Anya <bearduckparty@gmail.com>
2026-01-24 09:51:02 -0500

    Updated to math Elvis' code

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -1,17 +1,6 @@
 package frc.robot.subsystems;
 
 import frc.robot.LimelightHelpers;
-import frc.robot.subsystems.Shooter;
-
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-
-import java.nio.DoubleBuffer;
-
-import com.revrobotics.spark.SparkMax;
-
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
 
 
 
@@ -21,18 +10,22 @@ public class Turret {
 
     public static double[] aimAndRange() {
         double tx = LimelightHelpers.getTX("limelight");
-        double kP = .18; //subject to change
+        double kP = 0.035; //subject to change
 
         double angularVel = tx * kP;
 
-        double[] limelightSpeeds = NetworkTableInstance.getDefault().getTable("limelight").getEntry("targetpose_cameraspace").getDoubleArray(new double[6]);
+        double rawForwardSpeeds = LimelightHelpers.getTY("limelight");
 
-        double horizontalSpeeds = limelightSpeeds[0] * kP;
-        double forwardSpeeds = limelightSpeeds[2] * kP;
+        double error = 3.0 - rawForwardSpeeds;
 
-        double[] speeds = {horizontalSpeeds, forwardSpeeds, -angularVel};
+        if (Math.abs(error) < 0.05) {
+            error = 0;
+        }
 
-        return speeds;
+        double forwardSpeeds = error *  kP;
+
+
+        return new double[] {forwardSpeeds, angularVel};
 
 
     }
